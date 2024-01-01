1a6e6a2435dddca7136495e7759c426e6a2741f2
alex <alex.liswandy@hotmail.com>
2024-01-02 00:03:42 +0800

    Routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Kanban from "./pages/Kanban";
+import Calendar from "./pages/Calendar";
 
 function App() {
   return (
-    <div>
-      <header>
-        <p>
-          Hello world
-        </p>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/">
+          <Route index Component={Kanban} />
+          <Route path="/kanban" Component={Kanban} />
+          <Route path="/calendar" Component={Calendar} />
+        </Route>
+      </Routes>
+
+      <Link to="/calendar">
+        <p>To calendar</p>
+      </Link>
+
+      <Link to="/kanban">
+        <p>To kanban</p>
+      </Link>
+    </Router>
   );
 }
 
---
+++ src/pages/Kanban.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Kanban() {
+  return (
+    <h1>Kanban</h1>
+  );
+}
+
+export default Kanban;
